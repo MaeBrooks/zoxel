3d60907da97db875244546d3f335410108f9a1ff
zoftie <home_in_the_rain+zoftie@proton.me>
2025-10-21 17:55:34 -0700

    Initialized a zig raylib project!

---
+++ src/main.zig
@@ -0,0 +1,15 @@
+const std = @import("std");
+const rl = @import("raylib");
+
+pub fn main() !void {
+  rl.initWindow(800, 450, "Vox");
+  defer rl.closeWindow();
+
+  while (!rl.windowShouldClose())
+    {
+      rl.beginDrawing();
+      defer rl.endDrawing();
+      rl.clearBackground(rl.getColor(0xFFAAFFFF));
+      rl.drawText("Hai :3", 190, 200, 20, rl.getColor(0x000000FF));
+  }
+}
